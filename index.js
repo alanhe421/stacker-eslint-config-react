2fd2fc4243642d4aea0bc483d8fc96cb9f3ffe4e
Alan He <alan@1991421.cn>
2021-01-24 16:10:48 +0800

    feat(lint): lint for redux-saga

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ module.exports = {
     },
     parser: '@typescript-eslint/parser',
     plugins: [
-        '@typescript-eslint'
+        '@typescript-eslint',
+        'redux-saga'
     ],
     extends: [
         'eslint:recommended',
